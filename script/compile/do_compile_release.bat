a9a86a9702be41b52a0684134882d0ffded6e828
Juan Manuel Fernández Muñoz <juanmanuel.fernandez@iertec.com>
2023-02-18 14:24:56 +0100

    Changes to script: - Added scripts to compile the Debug and Release build configurations of the project.

---
+++ script/compile/do_compile_release.bat
@@ -0,0 +1,13 @@
+@echo off
+
+cd /d %~dp0
+
+call ..\global.bat
+
+set PATH=%PATH_IDE%;%PATH%
+
+cd ..\..\
+
+stm32cubeidec --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data %cd% -cleanBuild %PROJECT_NAME%/Release
+
+pause

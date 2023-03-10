054c5602ad362a5d724f720ce37fd683f45a46f5
Juan Manuel Fernández Muñoz <juanmanuel.fernandez@iertec.com>
2023-03-10 20:37:34 +0100

    Changes to script: - Added MISRA analysis to CppCheck.

--- script/static_analysis/do_analysis_cppcheck.sh
+++ script/static_analysis/do_analysis_cppcheck.sh
@@ -2,9 +2,19 @@ cd "$(dirname "$0")"
 
 source ../global.sh
 
+export PATH_CPPCHECK_SHARE
+
 PATH=$PATH_TARGET_COMPILER:$PATH
 PATH_BUILD=../../$PROJECT_NAME/Release
 PATH_OUT=../output/static_analysis
+MISRA_JSON_TEXT='
+{
+    "script": "'$(python3 -c "import os; print(os.environ['PATH_CPPCHECK_SHARE'])")'/addons/misra.py",
+    "args": [ "--rule-texts=misra-rules.txt" ]
+}'
+
+# Create the MISRA configuration file
+echo $MISRA_JSON_TEXT > $PATH_OUT/misra.json
 
 # CppCheck analysis
 # Note: Update compiler include paths "-I" from "script/output/co-co-arm-none-eabi-gcc.lnt".
@@ -12,6 +22,7 @@ CPPCHECK_ARG='
 --enable=all
 --xml
 --output-file='$PATH_OUT'/report_cppcheck.xml
+--addon='$PATH_OUT'/misra.json
 --platform='$PATH_CPPCHECK_SHARE'/platforms/arm32-wchar_t4
 -I"c:/st/stm32cubeide_1.11.0/stm32cubeide/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.10.3-2021.10.win32_1.0.100.202210260954/tools/bin/../lib/gcc/arm-none-eabi/10.3.1/../../../../arm-none-eabi/include/c++/10.3.1"
 -I"c:/st/stm32cubeide_1.11.0/stm32cubeide/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.10.3-2021.10.win32_1.0.100.202210260954/tools/bin/../lib/gcc/arm-none-eabi/10.3.1/../../../../arm-none-eabi/include/c++/10.3.1/arm-none-eabi"
